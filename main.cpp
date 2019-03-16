4621e8154c4a9a9689ba8362ea616a95b1ab0d45
landeaux <pluckedowl@gmail.com>
2019-03-15 19:26:40 -0700

    Modify threaded wait() to call non-threaded wait()

--- main.cpp
+++ main.cpp
@@ -1102,10 +1102,8 @@ void startSimulation(configMap config, metadataQueue mdQueue)
 void* wait(void* param)
 {
     float duration = *((float*)param);
-    Timer myTimer;
-
-    myTimer.startTimer();
-    while (myTimer.getDuration() < duration);
+    
+    wait(duration);
 
     pthread_exit(0);
 }
