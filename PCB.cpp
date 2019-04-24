4d83fee30dfd014efcf123fb0791d164e47460dd
landeaux <pluckedowl@gmail.com>
2019-04-24 14:49:10 -0700

    - Change name of setProgramCounter() and getProgramCounter() to setPC() and getPC(). - Implement copy constructor, overloaded assignement operator, and destructor. - Change default constructor to instantiate default parameters via initialization list. - Change copy constructor to instantate parameters via  initialization list and instantiate pc to 0.

--- PCB.cpp
+++ PCB.cpp
@@ -5,6 +5,17 @@
  * 
  * @details Implements all member methods of PCB class
  * 
+ * @version 1.03
+ *          Adam Landis (24 April 2019)
+ *          - Change name of setProgramCounter() and getProgramCounter() to 
+ *            setPC() and getPC().
+ *          - Implement copy constructor, overloaded assignement operator, and 
+ *            destructor.
+ *          - Change default constructor to instantiate default parameters via 
+ *            initialization list.
+ *          - Change copy constructor to instantate parameters via 
+ *            initialization list and instantiate pc to 0.
+ * 
  * @version 1.02
  *          Adam Landis (14 March 2019)
  *          - Implement setProgramCounter() and getProgramCounter() methods.
@@ -32,16 +43,45 @@
 /**
  * @brief      Constructs the object (default).
  */
-PCB::PCB() {}
+PCB::PCB() : pid(0), state(START), pc(0) {}
 
 /**
  * @brief      Constructs the PCB object (parameterized).
  */
-PCB::PCB(unsigned int pid)
+PCB::PCB(unsigned int pid) : pid(pid), state(START), pc(0) {}
+
+/**
+ * @brief      Copy constructor
+ *
+ * @param[in]  obj   The object to be copied
+ */
+PCB::PCB(const PCB &obj)
 {
-    this->state = START;
-    this->pid = pid;
+    this->pid = obj.pid;
+    this->pc = obj.pc;
+    this->state = obj.state;
 }
+
+/**
+ * @brief      Overloaded assignment operator
+ *
+ * @param[in]  rhs   The right hand side object to be copied
+ *
+ * @return     The copy of rhs PCB
+ */
+PCB& PCB::operator=(const PCB &rhs)
+{
+    this->pid = rhs.pid;
+    this->pc = rhs.pc;
+    this->state = rhs.state;
+
+    return *this;
+}
+
+/**
+ * @brief      Destroys the object.
+ */
+PCB::~PCB() {}
 
 /**
  * @brief      Sets the process state.
@@ -58,7 +98,7 @@ void PCB::setState(State state)
  *
  * @param[in]  pc    The address to set the program counter to (unsigned int)
  */
-void PCB::setProgramCounter(unsigned int pc)
+void PCB::setPC(unsigned int pc)
 {
     this->pc = pc;
 }
@@ -83,8 +123,12 @@ const unsigned int PCB::getPID() const
     return this->pid;
 }
 
-
-const unsigned int PCB::getProgramCounter() const
+/**
+ * @brief      Gets the program counter pc.
+ *
+ * @return     The value of the program counter pc.
+ */
+const unsigned int PCB::getPC() const
 {
     return this->pc;
 }
