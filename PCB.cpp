725bc8b4349ce3ea72b8c34c3d1c5f329ad2a06c
landeaux <pluckedowl@gmail.com>
2019-04-24 16:38:43 -0700

    - Remove all setters and getters - made them inline - Change constructors to initialize new data members numInstr and numIOInstr

--- PCB.cpp
+++ PCB.cpp
@@ -5,6 +5,12 @@
  * 
  * @details Implements all member methods of PCB class
  * 
+ * @version 1.04
+ *          Adam Landis (24 April 2019)
+ *          - Remove all setters and getters - made them inline
+ *          - Change constructors to initialize new data members numInstr and 
+ *            numIOInstr
+ * 
  * @version 1.03
  *          Adam Landis (24 April 2019)
  *          - Change name of setProgramCounter() and getProgramCounter() to 
@@ -43,12 +49,18 @@
 /**
  * @brief      Constructs the object (default).
  */
-PCB::PCB() : pid(0), pc(0), state(START) {}
+PCB::PCB() : pid(0), pc(0), numInstr(0), numIOInstr(0), state(START) {}
 
 /**
  * @brief      Constructs the PCB object (parameterized).
  */
-PCB::PCB(unsigned int pid) : pid(pid), pc(0), state(START) {}
+PCB::PCB(unsigned int pid, unsigned int numInstr, unsigned int numIOInstr) : 
+    pid(pid), 
+    pc(0), 
+    numInstr(numInstr), 
+    numIOInstr(numIOInstr), 
+    state(START)
+{}
 
 /**
  * @brief      Copy constructor
@@ -82,53 +94,3 @@ PCB& PCB::operator=(const PCB &rhs)
  * @brief      Destroys the object.
  */
 PCB::~PCB() {}
-
-/**
- * @brief      Sets the process state.
- *
- * @param[in]  state  The process state
- */
-void PCB::setState(State state)
-{
-    this->state = state;
-}
-
-/**
- * @brief      Sets the program counter.
- *
- * @param[in]  pc    The address to set the program counter to (unsigned int)
- */
-void PCB::setPC(unsigned int pc)
-{
-    this->pc = pc;
-}
-
-/**
- * @brief      Gets the process state.
- *
- * @return     The process state.
- */
-const State PCB::getState() const
-{
-    return this->state;
-}
-
-/**
- * @brief      Gets the pid.
- *
- * @return     The pid.
- */
-const unsigned int PCB::getPID() const
-{
-    return this->pid;
-}
-
-/**
- * @brief      Gets the program counter pc.
- *
- * @return     The value of the program counter pc.
- */
-const unsigned int PCB::getPC() const
-{
-    return this->pc;
-}
