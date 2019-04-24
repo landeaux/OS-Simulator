3f3368a363cc3f05b1bb471c3395a6afcbe16861
landeaux <pluckedowl@gmail.com>
2019-04-24 15:35:33 -0700

    - Add setter and getter methods for instrVector data member. - Make getter method for getPID() to be inline.

--- Process.h
+++ Process.h
@@ -5,10 +5,14 @@
  * 
  * @details Specifies all member methods of the Process class
  * 
+ * @version 1.02
+ *          Adam Landis (24 April 2019)
+ *          - Add setter and getter methods for instrVector data member.
+ *          - Make getter method for getPID() to be inline.
+ * 
  * @version 1.01
  *          Adam Landis (24 April 2019)
- *          - Add data member instrVector to hold all instructions for the 
- *            process
+ *          Add data member instrVector to hold all instructions for the process
  * 
  * @version 1.00
  *          Adam Landis (24 April 2019)
@@ -42,7 +46,40 @@ public:
     Process(const Process &obj);
     Process& operator=(const Process &rhs);
     ~Process();
-    const unsigned int getPID() const;
+
+    /************************      Setter methods     *************************/
+    /**
+     * @brief      Sets the instr vector.
+     *
+     * @param[in]  instrVector  The instr vector to copy
+     */
+    inline void setInstrVector(std::vector<MetadataInstruction> instrVector)
+    {
+        this->instrVector = instrVector;
+    }
+    /**************************************************************************/
+
+    /************************      Getter methods     *************************/
+    /**
+     * @brief      Gets the pid.
+     *
+     * @return     The pid.
+     */
+    inline const unsigned int getPID() const { return this->pid; }
+
+    /**
+     * @brief      Gets the instruciton from instrVector located at index idx.
+     *
+     * @param[in]  idx   The index
+     *
+     * @return     The instr.
+     */
+    inline const MetadataInstruction getInstr(unsigned idx) const
+    { 
+        return this->instrVector[idx];
+    }
+    /**************************************************************************/
+
 private:
     unsigned int pid;
     std::vector<MetadataInstruction> instrVector;
